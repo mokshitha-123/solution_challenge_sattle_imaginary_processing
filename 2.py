1ec6b12311bd9693c9f5cf82b8c241992570537e
mokshitha-123 <154515160+mokshitha-123@users.noreply.github.com>
2023-12-21 16:24:23 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt# Function to process satellite image
+def process_agricultural_image(image_path):
+    # Load satellite image
+    satellite_image =np.array( cv2.imread(image_path))
+
+    # Implement image processing techniques (e.g., cropping, resizing, normalization)
+
+    # Use clustering (K-Means) to identify different crops in the field
+    kmeans = KMeans(n_clusters=3)  # Adjust the number of clusters based on the crops you want to identify
+    print (satellite_image)
+    flattened_image = satellite_image.reshape((-1, 3))
+    kmeans.fit(flattened_image)
+    segmented_image = kmeans.cluster_centers_.astype(int)[kmeans.labels_].reshape(satellite_image.shape)
+
+    # Crop classification results
+    crop_classes = ['Crop 1', 'Crop 2', 'Crop 3']  # Add more crop classes as needed
+    unique_labels = np.unique(kmeans.labels_)
+    crop_distribution = [np.sum(kmeans.labels_ == label) for label in unique_labels]
+
+    # Calculate crop growth time, disease analysis, yield estimation, flood loss, etc.
+    # Implement additional analysis based on your requirements
+
+    # Display pie chart for crop distribution
+    plt.pie(crop_distribution, labels=crop_classes, autopct='%1.1f%%')
+    plt.title('Crop Distribution in Field')
+    plt.show()
+
+    # Return results
+    return crop_classes, crop_distribution, other_analysis_results
+
+# Example usage
+image_path = 'path/to/your/satellite/image.jpg'
+crop_classes, crop_distribution, other_analysis_results = process_agricultural_image(image_path)
+
+# Print results
+print(f"Crop Classes: {crop_classes}")
+print(f"Crop Distribution: {crop_distribution}")
+print(f"Other Analysis Results: {other_analysis_results}")
+# Function to process satellite image
+def process_agricultural_image(image_path):
+    # Load satellite image
+    satellite_image =np.array( cv2.imread(image_path))
+
+    # Implement image processing techniques (e.g., cropping, resizing, normalization)
+
+    # Use clustering (K-Means) to identify different crops in the field
+    kmeans = KMeans(n_clusters=3)  # Adjust the number of clusters based on the crops you want to identify
+    print (satellite_image)
+    flattened_image = satellite_image.reshape((-1, 3))
+    kmeans.fit(flattened_image)
+    segmented_image = kmeans.cluster_centers_.astype(int)[kmeans.labels_].reshape(satellite_image.shape)
+
+    # Crop classification results
+    crop_classes = ['Crop 1', 'Crop 2', 'Crop 3']  # Add more crop classes as needed
+    unique_labels = np.unique(kmeans.labels_)
+    crop_distribution = [np.sum(kmeans.labels_ == label) for label in unique_labels]
+
+    # Calculate crop growth time, disease analysis, yield estimation, flood loss, etc.
+    # Implement additional analysis based on your requirements
+
+    # Display pie chart for crop distribution
+    plt.pie(crop_distribution, labels=crop_classes, autopct='%1.1f%%')
+    plt.title('Crop Distribution in Field')
+    plt.show()
+
+    # Return results
+    return crop_classes, crop_distribution, other_analysis_results
+
+# Example usage
+image_path = 'path/to/your/satellite/image.jpg'
+crop_classes, crop_distribution, other_analysis_results = process_agricultural_image(image_path)
+
+# Print results
+print(f"Crop Classes: {crop_classes}")
+print(f"Crop Distribution: {crop_distribution}")
+print(f"Other Analysis Results: {other_analysis_results}")
